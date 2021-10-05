858651f67db8fd136716b3637ce17938843f70e1
Steve Stomp <steve@codeandcoding.com>
2021-10-05 16:24:48 +0200

    chore: initial commit

---
+++ python/main.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+import random
+import os
+import logging
+
+logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.NOTSET)
+logger = logging.getLogger()
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    logger.info('Started generating')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
