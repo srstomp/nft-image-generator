bf654b7964642657c78d6fb54056819027ef2510
Steve Stomp <steve@codeandcoding.com>
2021-10-08 21:48:01 +0200

    feat: save files

--- python/main.py
+++ python/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 import os
 import logging
+import json
 
 logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.NOTSET)
 logger = logging.getLogger()
@@ -12,43 +13,82 @@ ROOT_DIR = os.path.dirname(__file__)
 RESOURCES_DIR = f'{ROOT_DIR}/resources'
 DESTINATION_DIR = f'{ROOT_DIR}/generated'
 
+def combine(img, top):
+    img.paste(top, (0, 0), mask = top)
+    return img
+
+
 def get_random_color():
     return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 
-def change_image_color(src, org_color, new_color):
-    im = Image.open(src)
-    im = im.convert('RGBA')
+def change_image_color(src, org_hex_color, new_rgb_color):
+    '''
+    Change color of image
+
+    :param src: obj
+    :param org_hex_color: string
+    :param new_rgb_color: string
+    :return:
+    '''
+    im = src.convert('RGBA')
     data = np.array(im)  # "data" is a height x width x 4 numpy array
     red, green, blue, alpha = data.T  # Temporarily unpack the bands for readability
 
-    rgb = ImageColor.getrgb(org_color)
-    areas = (red == rgb[0]) & (blue == rgb[1]) & (green == rgb[2])
-    data[..., :-1][areas.T] = new_color  # Transpose back needed
+    rgb = ImageColor.getrgb(org_hex_color)
+    logger.debug(rgb)
+    areas = (red == rgb[0]) & (green == rgb[1]) & (blue == rgb[2])
+    data[..., :-1][areas.T] = new_rgb_color  # Transpose back needed
 
     return Image.fromarray(data)
 
 
+def generate_image_asset(item):
+    logger.debug(item)
+    img = Image.open(f'{RESOURCES_DIR}/{item["file"]}')
+    for c in item['colors']:
+        logger.debug(c)
+        img = change_image_color(img, c, get_random_color())
+
+    return img
+
+
 def generate_background():
-    return change_image_color(f'{RESOURCES_DIR}/bg.png', '#FFFFFF', get_random_color())
+    img = Image.open(f'{RESOURCES_DIR}/bg.png')
+    return change_image_color(img, '#FFFFFF', get_random_color())
 
 
 def save_image(img, name):
-    img = img.resize((1000, 1000),Image.NEAREST)
-    #img.save(f'{DESTINATION_DIR}/{name}.png')
-    img.show()
+    img = img.resize((576, 720),Image.LANCZOS)
+    img.save(f'{DESTINATION_DIR}/{name}.png')
+    #img.show()
 
 
 def generate_image(index):
     '''
+    Generate image. Steps:
+    - Generate image layers
+    - Combine images
+    - Save to file
+
     :param index: int
     :return:
     '''
     logger.debug(f'Fetch base image')
     image = generate_background()
 
-    image.show()
-    save_image(image, 'blabla')
+    f = open(os.path.join(os.path.dirname(__file__), 'resources/data.json'), 'r')
+    data = json.load(f)
+
+    weighted_choice = lambda s: random.choice(sum(([v] * wt for v, wt in s), []))
+
+    for i in data['assets']:
+        if i['type'] == 'root':
+            combine(image, generate_image_asset(i))
+        if i['type'] == 'mouth':
+            combine(image, generate_image_asset(random.choice(i['items'])))
+
+    save_image(image, f'{data["name"]}_{index}')
 
 
 def generate_batch(amount, file_name):
@@ -59,4 +99,4 @@ def generate_batch(amount, file_name):
 
 
 if __name__ == '__main__':
-    generate_batch(1, 'letter')
+    generate_batch(100, 'letter')
