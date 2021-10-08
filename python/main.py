60c1ae02039946e839c0df62aced8d7ebbbac2b3
Steve Stomp <steve@codeandcoding.com>
2021-10-08 11:40:26 +0200

    feat: Added background generator

--- python/main.py
+++ python/main.py
@@ -7,14 +7,57 @@ import logging
 logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.NOTSET)
 logger = logging.getLogger()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+ROOT_DIR = os.path.dirname(__file__)
+RESOURCES_DIR = f'{ROOT_DIR}/resources/'
 
 
-# Press the green button in the gutter to run the script.
+def change_image_color(source, ):
+    img = source.convert("RGB")
+
+    data = img.getdata()
+
+    new_image_data = []
+    for item in data:
+        # change all white (also shades of whites) pixels to yellow
+        if item[0] in list(range(190, 256)):
+            new_image_data.append((255, 204, 100))
+        else:
+            new_image_data.append(item)
+
+    # update image data
+    img.putdata(new_image_data)
+
+    # save new image
+    #img.save("test_image_altered_background.jpg")
+
+    # show image in preview
+    #img.show()
+
+
+def generate_background():
+    bg = Image.open(f'{RESOURCES_DIR}bg.png')
+    bg = alt
+
+
+
+
+def generate_image(index):
+    '''
+    :param index: int
+    :return:
+    '''
+    logger.debug(f'Fetch base image')
+
+
+def generate_batch(amount):
+    logger.info(f'Started creating {amount} images')
+    logger.info(RESOURCES_DIR)
+    for i in range(amount):
+        logger.info(f'Image {i} of {amount}')
+        generate_image(i)
+
+
 if __name__ == '__main__':
-    logger.info('Started generating')
+    generate_batch(100)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
